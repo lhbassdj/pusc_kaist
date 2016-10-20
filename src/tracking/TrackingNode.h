ef778c57ef662bcd0845d790cfcd8f0f77464506
Dongjin Kim <wking@kaist.ac.kr>
2016-10-20 12:58:49 +0900

    Add files via upload

--- src/tracking/TrackingNode.h
+++ src/tracking/TrackingNode.h
@@ -1,3 +1,5 @@
+ #pragma once
+
  /**
  *  This file is part of pusc_kaist.
  *
@@ -14,7 +16,10 @@
  *  You should have received a copy of the GNU General Public License
  *  along with pusc_kaist.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+#ifndef TRACKINGNODE_H
+#define TRACKINGNODE_H
+
 #include "ros/ros.h"
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
@@ -29,16 +34,33 @@
 #include <image_transport/image_transport.h>
 #include <pthread.h>
 #include "std_msgs/Empty.h"
-#include "TrackingNode.h"
 
-int main(int argc, char **argv)
+using namespace std;
+
+string intToString(int number);
+void on_trackbar(int, void*);
+void createTrackbars();
+void clickAndDrag_Rectangle(int event, int x, int y, int flags, void* param);
+void recordHSV_values(cv::Mat frame, cv::Mat hsv_frame);
+void drawObject(int x, int y, cv::Mat &frame);
+void morphOps(cv::Mat &thresh);
+void trackFilteredObject(int &x, int &y, cv::Mat threshold, cv::Mat &cameraFeed, double &currentArea, bool &found);
+void searchForMovement(cv::Mat thresholdImage, cv::Mat &cameraFeed);
+
+class ImageConverter
 {
-	ros::init(argc, argv, "drone_tracking");
+	//Codes are from cv_bridge tutorial.
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+	ros::Publisher land_pub;
+	ros::Subscriber land_sub;
 
-	ROS_INFO("Started TUM ArDrone Tracking Node.");
-
-	ImageConverter ic;
-	ros::spin();
-
-	return 0;
-}
+public:
+	ImageConverter();
+	~ImageConverter();
+	void landCb(std_msgs::EmptyConstPtr);
+	void imageCb(const sensor_msgs::ImageConstPtr& msg);
+};
+#endif
